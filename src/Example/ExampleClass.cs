eadba04e0ad4b5748d9f2dbd9a930a3f9d766df9
Nikolai Emil Damm <neq@energinet.dk>
2025-02-28 15:40:20 +0100

    feat(example): add ExampleClass and corresponding tests; remove Program.cs
    Signed-off-by: Nikolai Emil Damm <neq@energinet.dk>

---
+++ src/Example/ExampleClass.cs
@@ -0,0 +1,9 @@
+﻿namespace Example;
+
+/// <summary>
+/// An example class.
+/// </summary>
+public class ExampleClass
+{
+
+}
---
+++ tests/Example.Tests/ExampleClassTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Example.Tests;
+
+/// <summary>
+/// Tests for the <see cref="ExampleClass"/> class.
+/// </summary>
+public class ExampleClassTests
+{
+  /// <summary>
+  /// Test that the <see cref="ExampleClass"/> class is not null
+  /// </summary>
+  [Fact]
+  public void Test1()
+  {
+    // Arrange
+    var exampleClass = new ExampleClass();
+
+    // Act & Assert
+    Assert.NotNull(exampleClass);
+  }
+}
